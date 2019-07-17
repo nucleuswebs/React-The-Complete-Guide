34ff75b5ef7157a74fe0cc5e0ca08f094ec6caa2
Nucleus Websites <tamas.palecian@nucleuswebs.com>
2019-07-17 17:56:03 +0300

    updated stable version

---
+++ src/Application.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default class Application extends React.Component
+{
+
+    componentWillMount() {
+    }
+
+    componentDidMount()
+    {
+
+    }
+
+    render() {
+        return (
+            <div>
+                This is the application
+            </div>
+        )
+    }
+}
