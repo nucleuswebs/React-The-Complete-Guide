5b328ea93ed611a3edd3819508f709eb53a81e39
Nucleus Websites <tamas.palecian@nucleuswebs.com>
2019-07-23 18:12:34 +0300

    working on blog

--- src/Application.jsx
+++ src/Application.jsx
@@ -1,5 +1,6 @@
 import React, {Fragment} from 'react';
 import axios from 'axios';
+
 import HeaderComponent from "./components/Header.component";
 
 export default class Application extends React.Component
@@ -7,25 +8,9 @@ export default class Application extends React.Component
     constructor(props)
     {
         super(props);
-
-        this.handleSearchVideoByTerm = this.handleSearchVideoByTerm.bind(this);
     }
 
     componentDidMount() {
-    }
-
-    async handleSearchVideoByTerm( term ) {
-        const response = await axios.get('https://www.googleapis.com/youtube/v3/search', {
-            params: {
-                q: term,
-                part: "snippet",
-                key: "AIzaSyC-bNL2Hrd7MXuLzHJb9vS2RoiJhOMUK88",
-                maxResults: 25,
-            }
-        });
-
-        console.log(response);
-        // you will have to do something with the response
     }
 
     render() {
