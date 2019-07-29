5106cedabaed6464eab3b9391c629f3284d246dd
Nucleus Websites <tamas.palecian@nucleuswebs.com>
2019-07-29 19:55:58 +0300

    final version

--- src/store/actions/Todo.actions.js
+++ src/store/actions/Todo.actions.js
@@ -1,7 +1,66 @@
-export const testDispatch = () => {
-    return (dispatch) => {
+import axios from 'axios';
+
+export const getTodoList = () => {
+    return function( dispatch ) {
+        axios.get('https://jsonplaceholder.typicode.com/todos')
+            .then((response) => {
+                dispatch({
+                    type: 'GET_TODO_LIST',
+                    data: response.data
+                })
+            })
+    }
+};
+
+export const toggleTodoStatus = ( todoId ) => {
+    return function( dispatch ) {
         dispatch({
-            type: 'Test'
+            type: 'TOGGLE_TODO_ITEM',
+            data: todoId
         })
     }
-}
+};
+
+export const filterTodoByStatus = ( status ) => {
+    return function( dispatch ) {
+        dispatch({
+            type: 'FILTER_TODO_ITEMS_BY_STATUS',
+            data: status
+        })
+    }
+};
+
+export const selectTodoItem = ( itemId ) => {
+    return function( dispatch ) {
+        dispatch({
+            type: 'SELECT_TODO_ITEM',
+            data: itemId
+        })
+    }
+};
+
+export const changeTodoItemTitle = ( title ) => {
+    return function(dispatch) {
+        dispatch({
+            type: 'EDIT_TODO_ITEM_TITLE',
+            data: title,
+        })
+    }
+};
+
+export const changeTodoItemStatus = ( status ) => {
+    return function(dispatch) {
+        dispatch({
+            type: 'EDIT_TODO_ITEM_STATUS',
+            data: status,
+        })
+    }
+};
+
+export const updateTodoItem = () => {
+    return function(dispatch) {
+        dispatch({
+            type: 'UPDATE_TODO_ITEM',
+        });
+    }
+};
--- src/store/reducers/Todo.reducer.js
+++ src/store/reducers/Todo.reducer.js
@@ -1,7 +1,89 @@
-const initialState = {};
+const initialState = {
+    items: [],
+    selectedItem: null,
+    total: 0,
+};
 
 const todos = ( state = initialState, action ) => {
     switch ( action.type ) {
+        case 'GET_TODO_LIST':
+            const retrievedItems = action.data;
+            retrievedItems.map((value,key) => {
+                value.isVisible = true;
+                return value;
+            });
+
+            return {
+                ...state,
+                items: action.data,
+                total: action.data.length
+            };
+        case 'TOGGLE_TODO_ITEM':
+            let todoItems = state.items.map((item, key) => {
+               if (item.id === action.data) {
+                   item.completed = !item.completed;
+               }
+
+               return item;
+            });
+
+            return {
+                ...state,
+                items: todoItems,
+            };
+        case 'FILTER_TODO_ITEMS_BY_STATUS':
+            let filterStatus = action.data === 'uncompleted' ? false : true;
+
+            let filteredItems = state.items.map((value,key) => {
+                if (action.data === 'all') {
+                    value.isVisible = true;
+                    return value;
+                }
+
+                value.isVisible = value.completed === filterStatus;
+                return value;
+            });
+
+            return {
+                ...state,
+                items: filteredItems,
+            };
+        case 'SELECT_TODO_ITEM':
+            const selectedItem = state.items.filter((value, key) => {
+                return value.id === action.data
+            });
+            return {
+                ...state,
+                selectedItem: selectedItem[0]
+            };
+        case 'EDIT_TODO_ITEM_TITLE':
+            let editedSelectedItem = state.selectedItem;
+            editedSelectedItem.title = action.data;
+
+            return {
+                ...state,
+                selectedItem: editedSelectedItem,
+            };
+        case 'EDIT_TODO_ITEM_STATUS':
+            let editedStatusItem = state.selectedItem;
+            editedStatusItem.completed = action.data;
+
+            return {
+                ...state,
+                selectedItem: editedStatusItem,
+            };
+        case 'UPDATE_TODO_ITEM':
+            let updatedItemsList = state.items.map((value,key) => {
+                if (value.id === state.selectedItem.id) {
+                    value = state.selectedItem;
+                }
+                return value;
+            });
+
+            return {
+                ...state,
+                items: updatedItemsList
+            };
         default:
             return state;
     }
